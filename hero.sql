1ed8b71a3cddddae016aee343e8b0b469c480e59
kngjrm <lacernakingjerome@gmail.com>
2023-11-06 21:53:12 +0800

    feat select active player syntax

--- hero.sql
+++ hero.sql
@@ -166,3 +166,9 @@ WHERE hero_id = 1;
 
 DELETE FROM HeroItem 
 WHERE hero_id = 1;
+
+-- 5. Create new branch named "feat/select-active-players"
+SELECT p.player_name, h.hero_name
+FROM Player p
+JOIN Hero h ON p.hero_id = h.hero_id
+WHERE h.is_active = 'true';
